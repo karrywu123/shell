4aa15a93d425a5705933f28015c6fb994bf3e6fa
karrywu <38031335+karrywu123@users.noreply.github.com>
2022-01-23 16:44:32 +0700

    Update WangsuCDN.go

--- WangsuCDN.go
+++ WangsuCDN.go
@@ -6,8 +6,10 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"github.com/mkideal/cli"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strings"
 	"time"
 )
@@ -18,7 +20,7 @@ func Sha1(apikey ,nowtime string) string {
 	return base64.StdEncoding.EncodeToString(hmac.Sum(nil))
 }
 
-func WangSuCDNpreu(){
+func WangSuCDNpreu(domain,username,apikey string){
 	client := &http.Client{}
 	var values map[string]string
 	values = make(map[string]string)
@@ -26,8 +28,8 @@ func WangSuCDNpreu(){
 	values["Date"]=time.Now().UTC().Format(http.TimeFormat)
 	var values1 map[string]interface{}
 	values1 = make(map[string]interface{})
-	values1["dirs"]=[]string{"https://网宿域名/"}
-	token :=Sha1("网宿的apikey",time.Now().UTC().Format(http.TimeFormat))
+	values1["dirs"]=[]string{"https://"+domain+"/"}
+	token :=Sha1(apikey,time.Now().UTC().Format(http.TimeFormat))
 
 	js,_ := json.Marshal(values1)
 	urls :="https://open.chinanetcenter.com/ccm/purge/ItemIdReceiver"
@@ -36,7 +38,7 @@ func WangSuCDNpreu(){
 	for key,value := range values {
 		req.Header.Add(key,value)
 	}
-	req.SetBasicAuth("网宿用户",token)
+	req.SetBasicAuth(username,token)
 	resp, err := client.Do(req)
 	defer resp.Body.Close()
 	content, err := ioutil.ReadAll(resp.Body)
@@ -46,6 +48,19 @@ func WangSuCDNpreu(){
 
 	fmt.Println(string(content))
 }
+type argT struct {
+	cli.Helper
+	Domain string `cli:"d,domain" usage:"输入网宿刷新的域名"`
+	Apikey string  `cli:"k,apikey" usage:"输入网宿apikey"`
+	Username string  `cli:"u,username" usage:"输入网宿的账号名称"`
+}
+
 func main() {
-	WangSuCDNpreu()
+	os.Exit(cli.Run(new(argT), func(ctx *cli.Context) error {
+		argv := ctx.Argv().(*argT)
+		ctx.String("domain=%s, apikey=%s, username=%s\n", argv.Domain, argv.Apikey, argv.Username)
+		fmt.Println("刷新的域名为： "+argv.Domain+"刷新的网宿账号为：" +argv.Username)
+		WangSuCDNpreu(argv.Domain,argv.Username,argv.Apikey)
+		return nil
+	}))
 }
